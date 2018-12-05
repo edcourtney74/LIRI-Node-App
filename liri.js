2b89a3461d41566484ac30613faadcc4dd2b8d0a
Ed Courtney <edcourtney74@gmail.com>
2018-12-05 08:46:01 -0500

    Added functionality to append request and results to log.txt

--- liri.js
+++ liri.js
@@ -1,7 +1,3 @@
-// Left to do:
-// Add in error message alerts
-
-
 // Configure dotenv package
 require("dotenv").config();
 
@@ -23,7 +19,17 @@ var userChoice = process.argv.splice(3).join(" ");
 // Capitalize userChoice if user entered something
 if (userChoice) {
     userChoice = userChoice.charAt(0).toUpperCase() + userChoice.slice(1);
+}
+
+// Concatenate task and userChoice to add to log.txt
+var userCommand = "\n===============================\n\n" + task + ": " + userChoice + "\n";
+
+// Append command text to log.txt
+fs.appendFile("log.txt", userCommand, function (err) {
+    if (err) {
+        console.log(err);
     }
+})
 
 // FUNCTIONS==============================================================
 // Function to run when user enters concert-this
@@ -34,26 +40,51 @@ function getConcertInfo() {
 
         // Once API response is received...
         .then(function (response) {
+
+            // Variable for concert title
+            var concertTitle = "\n" + userChoice + "'s upcoming shows\n"
             
             // Console.log a title for the results
-            console.log(userChoice + "'s next 10 shows");
-            
+            console.log(concertTitle);
+
+            // Append title to log.txt
+            fs.appendFile("log.txt", concertTitle, function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            })
+
             // Do a for loop to go through the first 10 items returned by the API
             for (i = 0; i < 9; i++) {
                 // Set a variable for venue section of the response
                 var venue = response.data[i].venue;
 
-                // Info from response data to display in console log
-                console.log("--------------------")
-                console.log("Venue: " + venue.name);
-                console.log("Location: " + venue.city + ", " + venue.region);
-                // Use moment.js to convert date format
-                console.log("Date: " + moment(response.data[i].datetime).format("MM/DD/YY"));
+                // Create variable of all results
+                var resultsText = "--------------------\nVenue: " 
+                + venue.name + "\nLocation: " 
+                + venue.city + ", " + venue.region
+                // Use moment to convert to MM/DD/YY format 
+                + "\nDate: " + moment(response.data[i].datetime).format("MM/DD/YY") + "\n";
+
+                console.log(resultsText);
+
+                // Append results to log.txt
+                fs.appendFile("log.txt", resultsText, function (err) {
+                    if (err) {
+                        console.log(err);
+                    }
+                })
             }
         })
-        
         .catch(function (error) {
-            console.log("There is no concert information for the band/person you are looking for.")
+            // Display error to user in console
+            console.log("I'm sorry. I couldn't find concert information for " + userChoice + ".");
+            // Append error results to log.txt
+            fs.appendFile("log.txt", "I'm sorry. I couldn't find concert information for " + userChoice + ".", function(err) {
+                if (err) {
+                    console.log(err);
+                }
+            })
         });
 }
 
@@ -64,26 +95,32 @@ function getSongInfo() {
         id: keys.spotify.id,
         secret: keys.spotify.secret
     });
-    
+
     // If the user didn't enter a song, make userChoice "Mr. Nobody"
     if (!userChoice) {
         userChoice = "The Sign Ace of Base"
     }
-    
+
     // Search for song using node-spotify-api
-    spotify.search({ type: "track", query: userChoice }, function(err, response) {
+    spotify.search({ type: "track", query: userChoice }, function (err, response) {
         // Display message if error is received
         if (err) {
-        console.log("I'm sorry. The song you entered could not be found.");
-    }
+            console.log("I'm sorry. The song you entered could not be found.");
+            fs.appendFile("log.txt", "I'm sorry. The song you entered could not be found.", function() {
+                if (err) {
+                    console.log(err);
+                }
+            })
+        }
         // Variable to store temporary response object
         var songObj = response.tracks.items[0]
 
         // Console.log info to the terminal
+        console.log("--------------------")
         console.log("Song: " + songObj.name);
         console.log("Artist(s): " + songObj.artists[0].name);
         console.log("Album: " + songObj.album.name);
-        console.log("Preview URL: " + songObj.preview_url);        
+        console.log("Preview URL: " + songObj.preview_url);
     });
 }
 
@@ -113,6 +150,10 @@ function getMovieInfo() {
             console.log("Language: " + res.Language);
             console.log("Plot: " + res.Plot);
             console.log("Actors: " + res.Actors);
+        })
+        // Set a catch function to long an error if nothing is found
+        .catch(function (error) {
+            console.log("I'm sorry. We couldn't find any information for that movie.")
         });
 }
 
@@ -125,7 +166,6 @@ function taskSelected() {
             break;
 
         case "spotify-this-song":
-            console.log("Spotify this!")
             getSongInfo();
             break;
 
@@ -136,12 +176,12 @@ function taskSelected() {
 
         case "do-what-it-says":
 
-        // Read the random.txt file
+            // Read the random.txt file
             fs.readFile("random.txt", "utf8", function (error, data) {
 
                 // If code experiences errors, log it to the console
                 if (error) {
-                    return console.log(error);
+                    return console.log("I'm sorry. There seems to be a problem.");
                 }
 
                 // Split data by commas into a new array
@@ -149,7 +189,7 @@ function taskSelected() {
 
                 // Set array items to variables
                 task = dataArr[0];
-                
+
                 // Set second item in array as userChoice
                 userChoice = dataArr[1];
 
